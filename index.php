757f6b3f06dff824840164c0ba6d5c3f9a519a1b
jueon an <aje51488815@gmail.com>
2019-07-01 20:11:30 +0900

    php test

--- index.php
+++ index.php
@@ -1,11 +1,24 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<title>제목</title>
-</head>
-<body>
-<h1>This is a Title</h1>
-<h2>This is a second title</h2>
-<h3>This is a third title</h3>
-</body>
+
+<?php
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
+
+    $conn = new mysqli($server, $username, $password, $db);
+    echo "<h3>This is a php embbeded title</h3>";
+?>
+
+    <html>
+        <head>
+	        <title>제목</title>
+        </head>
+    <body>
+        <h1>This is a Title</h1>
+        <h2>This is a second title</h2>
+        <h3>This is a third title</h3>
+    </body>
 </html>
