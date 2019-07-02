f9a945834885eec88929e3cfcd39c1581048f5e5
jueon an <aje51488815@gmail.com>
2019-07-02 22:02:10 +0900

    ~~님 환영합니다

--- main.php
+++ main.php
@@ -17,10 +17,25 @@
     </head>
     <body>
         <h1><a href="index.php">MinsaPay</a></h1>
-        <h2>(Authorized User Only)</h2>
+        <h4>(Authorized User Only)</h4>
+        <?php
+            $id = $_SESSION['userid'];
+            $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-        contents..... differs by user
+            $server = $url["host"];
+            $username = $url["user"];
+            $password = $url["pass"];
+            $db = substr($url["path"], 1);
+        
+            $mysqli=mysqli_connect($server, $username, $password, $db);
+            
+            $check="SELECT * FROM user_info WHERE userid='$id'";
+            $result=$mysqli->query($check); 
+            $row=$result->fetch_array(MYSQLI_ASSOC);
+            $boothname = $row['boothname'];
 
+            echo "<h4>{$boothname} 님 환영합니다.</h4>";
+        ?>
         <br>
         <button type="button" onclick="location.href='logout.php' ">로그아웃</button>
     </body> 
