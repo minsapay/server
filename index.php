9705e5e86481649527d086f941092e7fc3721cdb
jueon an <aje51488815@gmail.com>
2019-07-03 18:17:51 +0900

    학번을 입력하세요

--- index.php
+++ index.php
@@ -8,19 +8,12 @@
             <h1><a href="index.php">MinsaPay</a></h1>
 
             <form method="POST" action="balance_check.php">
-                잔액 확인: 학번 입력
+                잔액 및 거래기록 확인:
                 <br>
                 <input type="number" name="studentid" value="Student ID number">
-                <input type="submit" value="잔액 확인">
+                <input type="submit" value="학번을 입력하세요">
             </form>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
+            <br> <br><br><br><br> <br><br><br>
             <button type="button" onclick="location.href='main.php' ">Authorized User Only (로그인 필요)</button>
 
         </body> 
