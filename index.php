edef74cc7b7733e6bc858abb0bd31568b6ab14c6
Sunghyun Cho <anaclumos@gmail.com>
2019-07-03 19:06:49 +0900

    메인 화면 잔액 확인 수정

--- index.php
+++ index.php
@@ -8,10 +8,8 @@
             <h1><a href="index.php">MinsaPay</a></h1>
 
             <form method="POST" action="balance_check.php">
-                잔액 및 거래기록 확인:
-                <br>
-                <input type="number" name="studentid" value="Student ID number">
-                <input type="submit" value="학번을 입력하세요">
+                <input type="number" name="studentid" placeholder = "학번을 입력해주세요" value="Student ID number">
+                <input type="submit" value="잔액 확인 💵">
             </form>
             <br> <br><br><br><br> <br><br><br>
             <button type="button" onclick="location.href='main.php' ">Authorized User Only (로그인 필요)</button>
