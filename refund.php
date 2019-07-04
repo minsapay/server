e5175bce8612a6c895e7ae94ad64e392909a049c
jueon an <aje51488815@gmail.com>
2019-07-05 02:37:06 +0900

    refund

---
+++ refund.php
@@ -0,0 +1,45 @@
+<?php
+    session_start();
+
+    //세션이 존재하지 않을 때 == 로그인이 아직 안 되어 있을 때
+    if(!isset($_SESSION['userid'])) 
+    {
+        header ('Location: ./main.php');
+    }
+    //세션이 존재할 때 == 로그인이 되어 있을 때
+    $id = $_SESSION['userid'];
+
+    require('db.php');
+    
+    $check="SELECT * FROM user_info WHERE userid='$id'";
+    $result=$mysqli->query($check); 
+    $row=$result->fetch_array(MYSQLI_ASSOC);
+    $boothname = $row['boothname'];
+    $isAdmin = $row['admin'];
+
+    //일반 부스 운영자가 들어왔을 때: 자기 위치로 이동
+    if(!$isAdmin)
+    {
+        header ('Location: ./main.php');
+    }
+?>
+<!DOCTYPE html>
+    <html>
+        <head>
+            <link href="https://fonts.googleapis.com/css?family=Noto+Sans+KR&display=swap" rel="stylesheet">
+            <link rel="stylesheet" type="text/css" href="MinsaPayDesignSystem.css">
+            <meta charset="utf-8">
+	        <title>민사페이</title>
+        </head>
+        <body>
+            <h1><a href="index.php">민사페이</a></h1>
+            <h3>잔액 환불 (행정위 전용 페이지)</h3>
+            <form method="POST" id="refund" action="refund_check.php" autocomplete="off" >
+                <input type="number" name="rfid" placeholder = "RFID를 입력해주세요" value="RFID number"> <br>
+            </form>
+            <button type="submit" class = "button1" form="refund" value="잔액 환불">잔액 환불</button>
+        </body> 
+    <h6>
+        © 닷넷. 모든 권리 보유.
+    </h6>
+    </html>
---
+++ refund_check.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+    <html>
+    <head>
+        <link href="https://fonts.googleapis.com/css?family=Noto+Sans+KR&display=swap" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="MinsaPayDesignSystem.css">
+        <meta charset="utf-8">
+        <title>민사페이</title>
+    </head>
+</html>
+<?php
+    session_start();
+
+    //세션이 존재하지 않을 때 == 로그인이 아직 안 되어 있을 때
+    if(!isset($_SESSION['userid'])) 
+    {
+        header ('Location: ./main.php');
+    }
+    //세션이 존재할 때 == 로그인이 되어 있을 때
+    $id = $_SESSION['userid'];
+
+    require('db.php');
+    
+    $check="SELECT * FROM user_info WHERE userid='$id'";
+    $result=$mysqli->query($check); 
+    $row=$result->fetch_array(MYSQLI_ASSOC);
+    $boothname = $row['boothname'];
+    $isAdmin = $row['admin'];
+
+    //일반 부스 운영자가 들어왔을 때: 자기 위치로 이동
+    if(!$isAdmin)
+    {
+        header ('Location: ./main.php');
+    }
+    $rfid = $_POST["rfid"];
+    $check="SELECT * FROM account_info WHERE rfid='$rfid'";
+    $result=$mysqli->query($check); 
+    $row=$result->fetch_array(MYSQLI_ASSOC);
+
+    if($result->num_rows==0)
+    {
+        echo "등록되지 않은 카드입니다.";
+    }
+    else
+    {
+        $balance = $row['balance'];
+        $charge=mysqli_query($mysqli,"UPDATE account_info SET balance='0' WHERE rfid='$rfid'");
+        if($charge)
+        {
+            echo $idnum," 계좌에 남은 잔액", $balance, "원은 전부 환불 처리되었습니다.";
+            echo "<br><button class = \"button2\" onclick=\"location.href='main.php'\"> 돌아가기 </button>";
+        }
+        else
+        {
+            echo "환불에 실패했습니다.";
+            echo "<br><button class = \"button2\" onclick=\"location.href='main.php'\"> 돌아가기 </button>";
+        }
+    }
+?>
