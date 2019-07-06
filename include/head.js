6e34bcaa44060208d7afc14d9bb263f435de5f50
Sunghyun Cho <anaclumos@gmail.com>
2019-07-06 14:02:09 +0900

    오타 수정

--- include/head.js
+++ include/head.js
@@ -1,2 +1,2 @@
 document.write('<link rel="stylesheet" type="text/css" href="MinsaPayDesignSystem.css">');
-document.write('<meta name="viewport" content="width=device-width, initial-scale=1.0">);
+document.write('<meta name="viewport" content="width=device-width, initial-scale=1.0">');
