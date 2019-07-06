111e8868ca707ab75fe239ec552207693955e193
Sunghyun Cho <anaclumos@gmail.com>
2019-07-06 14:00:20 +0900

    모바일 뷰포트 추가

--- include/head.js
+++ include/head.js
@@ -1,1 +1,2 @@
- document.write('<link rel="stylesheet" type="text/css" href="MinsaPayDesignSystem.css">');
+document.write('<link rel="stylesheet" type="text/css" href="MinsaPayDesignSystem.css">');
+document.write('<meta name="viewport" content="width=device-width, initial-scale=1.0">);
