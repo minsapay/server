5a1d5dcd91c295b8e33c75d86f17089d2cfda412
jueon an <aje51488815@gmail.com>
2019-07-07 10:00:51 +0900

    내역 확인

--- balance_check.php
+++ balance_check.php
@@ -3,7 +3,18 @@
         <head>
             <script language="javascript" type="text/javascript" src="include/head.js"></script>
             <meta charset="utf-8">
-	        <title>민사페이 계좌 잔액 확인</title>
+            <title>민사페이 계좌 잔액 확인</title>
+            <script type="text/javascript">
+            var bDisplay = true;
+            function doDisplay(){
+                var con = document.getElementById("myDIV");
+                if(con.style.display=='none'){
+                    con.style.display = 'block';
+                }else{
+                    con.style.display = 'none';
+                }
+            }
+            </script>
         </head>
         <body>
             <script language="javascript" type="text/javascript" src="include/header.js"></script>
@@ -16,15 +27,60 @@
                 if($result->num_rows==0)
                 {
                     echo "등록되지 않은 계좌입니다.";
+                    exit();
                 }
-                else
-                {
-                    $balance = $row['balance'];
-                    echo "당신의 잔액은 ";
-                    echo "<h4>", $balance, "원</h4>";
-                    echo"입니다.";
-                }
+                $balance = $row['balance'];
+                $id = $row['idnumber'];
+                echo "당신의 잔액은 ";
+                echo "<h4>", $balance, "원</h4>";
+                echo"입니다.";
+                if($row[freepass])
+                    echo "<br>당신은 문기부 FREEPASS 대상자 입니다.";
             ?>
+            <a href="javascript:doDisplay();">> 결제 기록 보기</a><br/><br/>
+            <div id="myDIV">
+                <?php
+                    $query  = $mysqli->_query("SELECT * FROM transaction_list ORDER BY num asc WHERE who='$id';");
+                    $result = $mysqli->_fetch($query);
+                    ?>
+                    <thead>
+                    <tr>
+                      <th>시간</th>
+                      <th>계좌</th>
+                      <th>부스명</th>
+                      <th>분류</th>
+                      <th>잔액</th>
+                    </tr>
+                    </thead>
+                    <?php
+                    echo("<tbody>");
+                    for($i=0; $i<sizeof($result); $i++)
+                    {
+                        $count++;
+                        $time = $result[$i]->timestamp;
+                        $who =  $result[$i]->who;
+                        $booth =  $result[$i]->booth;
+                        switch($result[$i]->what)
+                        {
+                            case 0:
+                            $what = "개설";
+                            break;
+                            case 1:
+                            $what = "충전";
+                            break;
+                            case 2:
+                            $what = "결제";
+                            break;
+                            case 3:
+                            $what = "반납";
+                            break;
+                        }
+                        $balance =  $result[$i]->balance;
+                        echo("<tr><td>".$time."</td><td>".$who."</td><td>".$booth."</td><td>".$what."</td><td>".$balance."</td></tr>");
+                    }
+                    echo("</tbody>");
+                ?>
+            </div>
             <script language="javascript" type="text/javascript" src="include/footer.js"></script>
         </body>
     </html>
